449eac07cb885a3e46a3d9d098a835d08bbdd218
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2024-07-14 22:22:05 +0900

    [Bronze II] Title: 분해합, Time: 1116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,11 +1,14 @@
 import sys
+input = sys.stdin.readline
 
 N = int(input())
-result = []
 
-for num in range(1, N+1):
-    sum_num = sum(int(s) for s in str(num))
-    if N == num + sum_num:
-        result.append(num)
+for num in range(1, N+1):                               # 생성자는 반드시 분해합보다 작은 숫자
+    disassemble_sum = num + sum(map(int, str(num)))     # 해당 숫자의 분해합을 구해서
 
-print(min(result) if result else 0)
+    if disassemble_sum == N:                            # 목표하는 숫자와 같다면
+        print(num)                                      # 출력
+        break                                           # 찾았으면 break(작은 숫자부터 검토)
+
+else:                                                   # 다 돌았는데 못찾았다면
+    print(0)                                            # 0을 출력
