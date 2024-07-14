cc45835d16647091a85f4ffa1c8f783b6fe2ccaa
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2024-07-15 01:07:36 +0900

    [Bronze I] Title: 나무 조각, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.py
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -2,15 +2,10 @@ import sys
 
 array = list(map(int, sys.stdin.readline().split()))
 
-while True:
+while array != [1, 2, 3, 4, 5]:
     for i in range(1, 5):
         if array[i - 1] > array[i]:
-            temp = array[i - 1]
-            array[i - 1] = array[i]
-            array[i] = temp
+            array[i-1], array[i] = array[i], array[i-1]
             print(*array)
-            continue
         elif array == [1, 2, 3, 4, 5]:
             break
-    if array == [1, 2, 3, 4, 5]:
-        break
