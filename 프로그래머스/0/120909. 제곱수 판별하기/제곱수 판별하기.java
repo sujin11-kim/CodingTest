58b188fe41a29ad4fe2d83e9c2f9ad7e34576cbb
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-05-22 12:36:09 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+        int sqrt = (int) Math.sqrt(n);
+        if(sqrt*sqrt==n) return 1;
+        return answer;
+    }
+}
