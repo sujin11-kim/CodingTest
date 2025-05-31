9b0c9e70986b6e02bf5dcf90356d9e0c66bbd6c0
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-05-31 15:46:47 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int length = num_list.length;
+        int[] answer = new int[length+1];
+        
+        for(int i=0; i<length; i++) answer[i] = num_list[i];
+        
+        int comp = num_list[length-1] - num_list[length-2];
+        
+        if(comp > 0) answer[length] = comp;
+        else answer[length] = num_list[length-1]*2;
+        
+        return answer;
+    }
+}
