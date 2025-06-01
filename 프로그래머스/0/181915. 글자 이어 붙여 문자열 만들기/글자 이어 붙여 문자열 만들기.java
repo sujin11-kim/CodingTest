bd90e53106572168cc37163c295b00ac9e8e63a0
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-06-01 17:55:43 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 15.27 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        for(int idx: index_list) answer+=my_string.charAt(idx);
+        return answer;
+    }
+}
