b89cc58d05f5a7de57051e9e5533deecc4bf9221
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-06-02 16:09:36 +0900

    [level 0] Title: 배열 만들기 5, Time: 11.47 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> answer = new ArrayList<Integer>();
+        for(String str: intStrs) {
+            int num = Integer.parseInt(str.substring(s, s+l));
+            if(num>k) answer.add(num);
+        }
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
