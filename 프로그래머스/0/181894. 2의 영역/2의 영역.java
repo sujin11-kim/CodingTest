e8cc7a1cd60439b1a0b62a548c3d7fb26bc7796b
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-06-03 18:54:33 +0900

    [level 0] Title: 2의 영역, Time: 3.65 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int min_idx = 1000000;
+        int max_idx = -1;
+        
+        boolean flag = false;
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]==2) {
+                flag = true;
+                min_idx = Math.min(min_idx, i);
+                max_idx = Math.max(max_idx, i);
+            }
+        }
+        return flag ? Arrays.copyOfRange(arr, min_idx, max_idx+1) : new int[]{-1};
+    }
+}
