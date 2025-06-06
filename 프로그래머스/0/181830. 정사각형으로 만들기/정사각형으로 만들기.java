e3756171f296afa643a689fe0b1d8f321f301d68
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-06-06 12:26:38 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.13 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int len = Math.max(arr.length, arr[0].length);
+        int[][] answer = new int[len][len];
+        for(int i=0; i<arr.length; i++) {
+            answer[i] = Arrays.copyOf(arr[i], len);
+        }
+        return answer;
+    }
+}
