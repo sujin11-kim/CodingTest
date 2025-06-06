cf10e97f3f672828bb5645b428f158d03633da32
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-06-06 11:51:21 +0900

    [level 0] Title: l로 만들기, Time: 1076.44 ms, Memory: 392 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(char c: myString.toCharArray()) {
+            if(c<'l') answer += 'l';
+            else answer += c;
+        }
+        return answer;
+    }
+}
