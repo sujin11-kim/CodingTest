c1085d0271cabc90630ef742827e11fae2d1452c
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-06-18 19:53:10 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 428 ms, Memory: 28180 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+	public String solution(String s) {
+		String answer = "yes";
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(char c: s.toCharArray()) {
+			if(c=='(' || c=='[') stack.add(c);
+			else if(c==')') {
+				if(stack.isEmpty() || stack.pop()!='(') return "no";
+			} else if(c==']') {
+				if(stack.isEmpty() || stack.pop()!='[') return "no";
+			}
+		}
+		if(!stack.isEmpty()) answer = "no";
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			String s = sc.nextLine();
+			if(s.equals(".")) break;
+			else {
+				System.out.println(T.solution(s));
+			}
+		}
+		sc.close();
+	}
+}
