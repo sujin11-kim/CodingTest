a3a4e2ec7fe1013f9fc2f18e96bc41c4622210e4
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2025-07-11 14:47:39 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 196 ms, Memory: 17808 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,40 +4,36 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int row = sc.nextInt();
-		int col = sc.nextInt();
-		sc.nextLine();
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 		
-		char[][] board = new char[row][col];
-		
-		for(int i=0; i<row; i++) {
-			String s = sc.nextLine();
-			for(int j=0; j<col; j++) {
+		int[][] board = new int[n][m];
+		for(int i=0; i<n; i++) {
+			String s = sc.next();
+			for(int j=0; j<m; j++) {
 				board[i][j] = s.charAt(j);
 			}
 		}
-	
+		
 		int answer = Integer.MAX_VALUE;
 		
-		for(int i=0; i<=row-8; i++) {
-			for(int j=0; j<=col-8; j++) {
-				int countB = 0, countW = 0;
+		for(int i=0; i<=n-8; i++) {
+			for(int j=0; j<=m-8; j++) {
+				int count1 = 0;
+				int count2 = 0;
 				for(int x=0; x<8; x++) {
 					for(int y=0; y<8; y++) {
-						if((x+y)%2==0) {
-							if(board[i+x][j+y]!='B') countB++;
-							if(board[i+x][j+y]!='W') countW++;	
-						} else {
-							if(board[i+x][j+y]!='W') countB++;
-							if(board[i+x][j+y]!='B') countW++;
-						}						
+						if((x+y)%2==0 && board[i+x][j+y]!='W') count1++;
+						else if((x+y)%2!=0 && board[i+x][j+y]!='B') count1++;
+						
+
+						if((x+y)%2==0 && board[i+x][j+y]!='B') count2++;
+						else if((x+y)%2!=0 && board[i+x][j+y]!='W') count2++;
 					}
 				}
-				answer = Math.min(answer, countB);
-				answer = Math.min(answer, countW);
+				answer = Math.min(answer, Math.min(count1, count2));
 			}
 		}
-		
 		System.out.print(answer);
 		sc.close();
 	}
